a7c791c0502572b20d527a499cbb8d8861c4f133
Dylan Mordaunt <15080672+edithatogo@users.noreply.github.com>
2026-04-19 19:40:53 +1000

    chore(rust): remove build artifacts from bindings scaffold

--- bindings/rust/target/debug/deps/architecture-eb57704526933832.d
+++
@@ -1,5 +0,0 @@
-/Users/doughnut/GitHub/innovate/innovate/bindings/rust/target/debug/deps/architecture-eb57704526933832.d: tests/architecture.rs
-
-/Users/doughnut/GitHub/innovate/innovate/bindings/rust/target/debug/deps/architecture-eb57704526933832: tests/architecture.rs
-
-tests/architecture.rs:
--- bindings/rust/target/debug/deps/scaffold-ff35e42a78db0a11.d
+++
@@ -1,5 +0,0 @@
-/Users/doughnut/GitHub/innovate/innovate/bindings/rust/target/debug/deps/scaffold-ff35e42a78db0a11.d: tests/scaffold.rs
-
-/Users/doughnut/GitHub/innovate/innovate/bindings/rust/target/debug/deps/scaffold-ff35e42a78db0a11: tests/scaffold.rs
-
-tests/scaffold.rs:
